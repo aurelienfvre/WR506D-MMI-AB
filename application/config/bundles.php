29c8220010ebcb14a931c3654be28c4b24f7b478
aurel <aurelieuxpro@gmail.com>
2024-10-24 09:07:05 +0200

    feat: add user authentication with JWT tokens

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -17,4 +17,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
